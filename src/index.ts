0c5c573a25c39ad6c57e4be2bcbc8b19dc609869
rock-tran <rock.tran@manadr.com>
2024-03-15 17:07:42 +0700

    Configure tsc-alias

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
 #! /usr/bin/env node
 
-import fs from "fs";
+import { StoryboardParser } from "./storyboard/storyboard_parser"
+
+
+const main = async () => {
+  const fileParser = new StoryboardParser(
+    "/mnt/Data/Workspace/4.MobileHealth/5.iOS/appointment-ios/MaNaDr/Modules/Clinic Visit/Detail/CVAppointmentDetail.storyboard"
+  )
+  await fileParser.convert()
+}
+main()
