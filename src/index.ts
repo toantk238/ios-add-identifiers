ccd77d8ccdadeb556510a79a1fa0db73e59129de
rock-tran <rock.tran@manadr.com>
2024-03-15 17:54:06 +0700

    feat : auto watch

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,21 @@
 #! /usr/bin/env node
 
-import { StoryboardParser } from "./storyboard/storyboard_parser"
+import yargs from "yargs";
+import { StoryboardParser } from "./storyboard/storyboard_parser";
 
+const argv: any = yargs
+  .usage("hello")
+  .command("gen", "Generate id of views")
+  .demandOption(["f"])
+  .nargs("f", 1)
+  .alias("f", "file")
+  .help("h")
+  .alias("h", "help").argv;
+
+const file = argv.file;
 
 const main = async () => {
-  const fileParser = new StoryboardParser(
-    "/mnt/Data/Workspace/4.MobileHealth/5.iOS/appointment-ios/MaNaDr/Modules/Clinic Visit/Detail/CVAppointmentDetail.storyboard"
-  )
-  await fileParser.convert()
-}
-main()
+  const fileParser = new StoryboardParser(file);
+  await fileParser.convert();
+};
+main();
