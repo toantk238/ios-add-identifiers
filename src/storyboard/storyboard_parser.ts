abbe270688b35721e152ad31948431d7cbb0f8e3
rock-tran <rock.tran@manadr.com>
2024-03-28 17:04:10 +0700

    feat : add 'id' for button

--- src/storyboard/storyboard_parser.ts
+++ src/storyboard/storyboard_parser.ts
@@ -1,9 +1,15 @@
 import { JSDOM } from "jsdom";
 import { readFile, writeFile } from "fs/promises";
-import { getChildNode } from "@utils/xml";
+import { getChildNode, getChildNodes } from "@utils/xml";
+import { inspect } from "util";
+import { exists } from "fs";
+import { count } from "console";
 
 export class StoryboardParser {
   private file: string;
+  private count: number = 0;
+  private existedIds = new Set();
+  private dom: JSDOM = new JSDOM();
 
   public constructor(file: string) {
     this.file = file;
@@ -16,33 +22,80 @@ export class StoryboardParser {
     //   ignoreAttributes: false,
     // });
     // const json = parser.parse(contentFile);
-    const dom = new JSDOM(contentFile, { contentType: "text/xml" });
-    const buttons = dom.window.document.querySelectorAll("button");
+    this.dom = new JSDOM(contentFile, { contentType: "text/xml" });
+    // missingIdentifiersViews.forEach((it) => {
+    //   console.log(it.outerHTML);
+    // });
+    this.insertId("button");
+    const xmlOutput = this.dom.serialize();
+    // console.log(`content = \n${xmlOutput}`);
+    const firstLine = '<?xml version="1.0" encoding="UTF-8"?>';
+    const modifiedContent = (firstLine + "\n" + xmlOutput + "\n")
+    await writeFile(this.file, modifiedContent);
+  }
+
+  insertId(tag: string) {
+    const buttons = this.dom.window.document.querySelectorAll(tag);
 
     // buttons.forEach((it) => {
     //   console.log(getChildNode(it, "rect").outerHTML);
     // });
 
     const missingIdentifiersViews = Array.from(buttons).filter((button) => {
-      const accessibility = getChildNode(button, "accessibility");
-      return accessibility == undefined;
+      getLocalizedKeyValue(button);
+      return undefinedIdentifier(button);
     });
 
     var index = 0;
-    const doc = dom.window.document;
+    const doc = this.dom.window.document;
     missingIdentifiersViews.forEach((it) => {
+      const localizedKey = getLocalizedKeyValue(it);
+      var suffix = "";
+      if (localizedKey) {
+        suffix = localizedKey;
+      } else {
+        suffix = "";
+      }
+      suffix = this.findAvailableSuffix(suffix);
+      const generatedId = `${tag}-${suffix}`;
       const newEl = doc.createElement("accessibility");
       newEl.setAttribute("key", "accessibilityConfiguration");
-      newEl.setAttribute("identifier", `button-${index}`);
+      newEl.setAttribute("identifier", generatedId);
       index++;
       it.appendChild(newEl);
     });
-
-    // missingIdentifiersViews.forEach((it) => {
-    //   console.log(it.outerHTML);
-    // });
-    const xmlOutput = dom.serialize();
-    console.log(`content = \n${xmlOutput}`);
-    await writeFile(this.file, xmlOutput);
   }
+
+  findAvailableSuffix(tag: string): string {
+    if (!this.existedIds.has(tag)) return tag;
+
+    var temp = tag;
+    while (this.existedIds.has(temp)) {
+      this.count++;
+      temp = `${temp}-${this.count}`;
+    }
+
+    return temp;
+  }
+}
+
+function undefinedIdentifier(node: Element) {
+  const accessibility = getChildNode(node, "accessibility");
+  return accessibility == undefined;
+}
+
+function getLocalizedKeyValue(node: Element): string | null | undefined {
+  const temp: any = getChildNode(node, "userDefinedRuntimeAttributes");
+  if (temp == undefined) return undefined;
+
+  const localizedKeys = getChildNodes(
+    temp,
+    "userDefinedRuntimeAttribute",
+  ).filter((x) => {
+    return x.getAttribute("keyPath") == "textLocalizedKey";
+  });
+  const localizedKeyTag =
+    localizedKeys.length > 0 ? localizedKeys[0] : undefined;
+
+  return localizedKeyTag?.getAttribute("value");
 }
--- src/utils/xml.ts
+++ src/utils/xml.ts
@@ -1,11 +1,18 @@
-export function getChildNode(
-  node: HTMLButtonElement,
-  tag: string,
-): ChildNode | undefined {
+export function getChildNode(node: Element, tag: string): Element | undefined {
   const childNodes = node.childNodes;
   for (let i = 0; i < childNodes.length; i++) {
     let item = childNodes[i];
-    if (item.nodeName == tag) return item;
+    if (item.nodeName == tag) return item as any;
   }
   return undefined;
+}
+
+export function getChildNodes(node: Element, tag: string): Element[] {
+  const result = [];
+  const childNodes = node.childNodes;
+  for (let i = 0; i < childNodes.length; i++) {
+    let item = childNodes[i];
+    if (item.nodeName == tag) result.push(item);
+  }
+  return result as any;
 }
